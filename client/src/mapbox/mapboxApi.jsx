472f173320ad6fc926b10fe07b6f8d86883f9416
fabnasiyas <fabnasiyasck@gmail.com>
2023-07-29 10:58:58 +0530

    updated

--- client/src/mapbox/mapboxApi.jsx
+++ client/src/mapbox/mapboxApi.jsx
@@ -1,14 +1,3 @@
-// import axios from 'axios';
-
-// const mapboxAPI = axios.create({
-//   baseURL: 'https://api.mapbox.com',
-//   params: {
-//     access_token: 'pk.eyJ1Ijoic2hpamFzMDkiLCJhIjoiY2xpaXUyZHQzMDFzeDNlcGEwbHd6ejJmOCJ9.TZzIUmMeUTVSKfdqqSWgWg' // Replace with your Mapbox access token
-//   }
-// });
-
-// export default mapboxAPI;
-
 
 
 import axios from 'axios';
