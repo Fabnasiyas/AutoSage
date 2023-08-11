d39d3f653e993745d1c40abf277038d8755450ce
fabnasiyas <fabnasiyasck@gmail.com>
2023-08-11 17:32:32 +0530

    middleware upadted

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({ extended: true, limit:"50mb" }));
 app.use(cors({ origin: ["http://localhost:3000","https://autosagenew.vercel.app"], credentials: true, }));
 app.use(cookieParser());
 app.use(express.static(path.resolve() + "/public"))
