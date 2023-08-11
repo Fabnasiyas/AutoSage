fc3253b99684cb3aa26d2346cdaa8ff0cdefb85b
fabnasiyas <fabnasiyasck@gmail.com>
2023-08-11 16:19:32 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import path from 'path';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:3000","https://autosage.sigag.online"], credentials: true, }));
+app.use(cors({ origin: ["http://localhost:3000","https://autosagenew.vercel.app/"], credentials: true, }));
 app.use(cookieParser());
 app.use(express.static(path.resolve() + "/public"))
 
